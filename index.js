7b2c52a67e09edf22f3ae14b30970ca78b2adec9
Traukor <fp.mathis@gmail.com>
2018-05-15 21:24:36 +0200

    c

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ var nextId;
 
 try {
     pool.getConnection(function(err, connection) {
-        connection.query('Select count(*) as rowCount from message', function (error, results, fields) {
+        connection.query('Select * as rowCount from message', function (error, results, fields) {
             if (error) console.log("error select count => " + error);
             var rows = JSON.parse(JSON.stringify(results));
-                console.log(rows);
-                nextId = rows.rowCount;
+            console.log(rows);
+            nextId = rows.rowCount;
             console.log("Dernier ID message => " + nextId);
         });
         connection.release();
