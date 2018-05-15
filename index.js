328deb9d14f2b5ec438b5cad8be4268a36647239
Traukor <fp.mathis@gmail.com>
2018-05-15 06:41:29 +0200

    c

--- index.js
+++ index.js
@@ -227,6 +227,7 @@ function InsertMessage(id,nbJour,channel,message)
 {
     var insert = process.env.insertMessage;
     insert = insert.replace('[ID]',id).replace('[NBJOUR]',nbJour).replace('[CHANNEL]',channel).replace('[MESSAGE]',message).replace('[TOGGLE]',1).replace('[CURRENTDAY]',0);
+    console.log(insert);
     connection.query(insert, function (error, results, fields) {
         if (error) console.log("erreur insert => " + error);
     });
