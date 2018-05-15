a4e19f63c084eeb06f220ed07107957104c756b8
Traukor <fp.mathis@gmail.com>
2018-05-15 06:27:03 +0200

    f

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ function ChangeGamePlayed()
 function InsertMessage(id,nbJour,channel,message)
 {
     var insert = process.env.insertMessage;
-    insert.replace('[ID]',id).replace('[NBJOUR]',nbJour).replace('[CHANNEL]',channel).replace('[MESSAGE]',message).replace('[TOGGLE]',1).replace('[CURRENTDAY]',0);
+    insert = insert.replace('[ID]',id).replace('[NBJOUR]',nbJour).replace('[CHANNEL]',channel).replace('[MESSAGE]',message).replace('[TOGGLE]',1).replace('[CURRENTDAY]',0);
     console.log("insert => " + insert);
     connection.query(insert, function (error, results, fields) {
         if (error) console.log("erreur insert => " + error);
